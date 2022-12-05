aa6c1b4d3c91cda8bdf5e8c367e89a2334af079a
AshleyTss <119179506+AshleyTss@users.noreply.github.com>
2022-12-05 21:38:05 +0800

    Add files via upload

---
+++ Directory_tree.py
@@ -0,0 +1,11 @@
+pip install directory_tree
+
+# Importing Libraries
+from directory_tree import display_tree
+display_tree()
+
+directoy_path=r'C:\Users\AshleyT\OneDrive - TCC\Documents\GitHub'
+
+# Main Method
+if __name__ == '__main__':
+    display_tree(directory_path)
