b53ea2bcd6a9b916ec92919d97921ed5725c047b
AshleyTss <119179506+AshleyTss@users.noreply.github.com>
2022-12-10 00:19:03 +0800

    Add files via upload

---
+++ Sharing file/client.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import socket
+
+host='127.0.0.1'
+port=5000
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.connect((host, port))
+
+message = s.recv(4096)
+
+while message:
+	print("Received:"+message.decode())
+	message = s.recv(4096)
+	
+s.close()
