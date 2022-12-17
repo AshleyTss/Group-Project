c3cc3a601045946c5c7d1de8cb1f870c15c7eab4
#Sheida <41925038+sheidaj@users.noreply.github.com>
2022-12-17 22:06:34 +0000

    Update and rename create a dictionary.py to create_dictionary.py

--- Sharing file/create_dictionary.py
+++ Sharing file/create_dictionary.py
@@ -1,32 +1,9 @@
 
-
 import pickle
 import json
 import socket
 
 
-"""
-## example 1
-## for a defined dictionary
-
-dic1={1: "Queen", 2: "London", 3:"UK"}
-dic2={'name': "Queen", 'city': "London", 'country':"UK"}
-
-print(dic1)
-print(dic2["country"])
-
-# convert dic1 to json 
-print(type(dic1))
-
-# convert into JSON:
-jdic1 = json.dumps(dic1)
-
-# result is a JSON string:
-print(jdic1)
-print(type(jdic1))
-"""
-
-## example 2
 ## to populate a dictionary from a loop
 
 # Creating an empty Dictionary
@@ -46,29 +23,7 @@ Dict = dict([(1, 'Queen'), (2, 'of')])
 print("\nDictionary with each item as a pair: ")
 print(Dict)
 
-"""
-## serlization and deserilization process
-## covnerting the dictionary to be transmitted
-
-dic4 = {"name": "Queen!", "city": "London"}
-# serialization
-# "wb" argument opens the file in binary mode
-with open("test.pickle", "wb") as outfile:
-    pickle.dump(dic4, outfile)
-print("Written dictionary", dic4)
-
-# to recover and deserialization
-with open("test.pickle", "rb") as infile:
-    dic4_recover = pickle.load(infile)
-print("Reconstructed object", dic4_recover)
-
-# to check the process
-if dic4 == dic4_recover:
-    print("Reconstruction  processed successfully")
-"""
-
-## serliazation and deserilization for example 3
-# serialization
+# serliazation and deserilization
 # "wb" argument opens the file in binary mode
 
 with open("test.pickle", "wb") as outfile:
