7875d7d8e6fb17839944f09642f6368cdf525ded
#Sheida <41925038+sheidaj@users.noreply.github.com>
2022-12-19 21:03:16 +0000

    Update unittest1.py

--- Testing/unittest1.py
+++ Testing/unittest1.py
@@ -1,10 +1,11 @@
 # this is to test the clinet server and dycrpted file
 
 import unittest
-import sys
+# import sys
 import unittest.mock
 
-
+from socket_server import *
+from socket_client import *
 from unittest.mock import Mock
 
 
@@ -23,32 +24,26 @@ class TestClient(unittest.TestCase):
         mock_dic_dirs('thing_to_create')
         mock_dic_dirs.assert_called_with('thing_to_create') 
     print("TBC")
-
-    # test the enrypted original file    
-    # actual = encrypt_data(original)
-    # expected = 'encrypted_text!'
-    #     self.assertEqual(actual, expected)
-
+    
+    
+    # test the enrypted outfile3    
+    # actual = decryoted file
+    # expected = 'encrypted_text as dict!'
+    # self.assertEqual(actual, expected)
+    
     def test_encrpyt(self):
         '''
         To test the serialisation process with no encrypt
         '''
-        actual = no_encrypt(original)
+        actual = decrypted
         expected = '{1: 'Queen', 2: 'of', 3: 'UK'}'
-        self.assertEqual(actual, expected,"serialise not successful")
-        print("serialise checked")
+        self.assertEqual(actual, expected,"serialise not success")
+        print("decrypted checked")
 
-    # def test_encrypt_input(self):
-    #     encrypt = 'a'
-    #     actual = 
-    #     expected = 'Please enter y or n'
-    #     if encrypt in ['y', 'n']:
-    #         return
-    #     else:
-    #         print('Please enter y or n')
-    #     self.assertEqual(actual, expected)
+    # test the pickling orinigal
+    
 
-    def test_encrypt_json_pickling(self):
+    def test_json_pickling(self):
         '''
         To test if the pickling choice returns a json type
         '''
@@ -57,7 +52,7 @@ class TestClient(unittest.TestCase):
             file_str = str(json.dumps(original))
         actual = file_str
         expected = '{1: 'Queen', 2: 'of', 3: 'UK'}'
-        self.assertEqual(actual, expected, "pickling not successful")
+        self.assertEqual(actual, expected, "pickling not success")
         print("pickling checked")
 
 
