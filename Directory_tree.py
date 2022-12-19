7bfef60561203d04027775bec732f9be7593b68e
sadienjb <sadienjb@gmail.com>
2022-12-19 20:41:23 +0400

    Update Directory_tree.py

--- Directory_tree.py
+++ Directory_tree.py
@@ -4,7 +4,7 @@
 from directory_tree import display_tree
 display_tree()
 
-directoy_path=r'C:\Users\AshleyT\OneDrive - TCC\Documents\GitHub'
+directory_path=r'C:\Users\AshleyT\OneDrive - TCC\Documents\GitHub'
 
 # Main Method
 if __name__ == '__main__':
